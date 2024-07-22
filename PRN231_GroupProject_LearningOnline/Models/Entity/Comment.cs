cb71600c67624e27d7b1056bca0bcfdf8f722d79
tung1221 <92321449+tung1221@users.noreply.github.com>
2024-07-22 09:55:18 +0700

    mode : add comment model

---
+++ PRN231_GroupProject_LearningOnline/Models/Entity/Comment.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PRN231_GroupProject_LearningOnline.Models.Entity
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string? Content { get; set; }
+        public DateTime? CreateDate { get; set; }
+        public int CourseId { get; set; }
+        public bool IsHide { get; set; }
+        public int ParentId { get; set; }
+
+    }
+}
